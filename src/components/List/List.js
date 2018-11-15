4a8f49e006c35e78f008bf158fdda60d10d5ff63
Andrew Harding <dreweharding@gmail.com>
2018-11-14 19:50:08 -0500

    notes display

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,20 +5,23 @@ import { Collapsible, CollapsibleItem } from "react-materialize";
 
 const List = props => {
     return (
-        <div>
             <Collapsible popout>
                 {props.user.list.map(item => {
                     return (
                         <CollapsibleItem header={item.city} icon="filter_drama">
                             <h5>Here's some info on {item.city}!</h5>
+                            {item.visited ? `You have visited ${item.city}!` : `You have not visited ${item.city} yet!`}
                             <ul>
-                                <li></li>
+                                <li>Weather:</li>
+                                <li>Current News:</li>
                             </ul>
+                            {item.notes.map(note => {
+                                return (<p>{note}</p>)
+                            })}
                         </CollapsibleItem>
                     );
                 })}
             </Collapsible>
-        </div>
     );
 };
 
