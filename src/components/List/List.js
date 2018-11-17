69e7745247eb0a32811e19517235fab809734a4b
Andrew Harding <dreweharding@gmail.com>
2018-11-17 11:29:04 -0500

    added note and city components, checkbox only returns false

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./List.css";
-import { Button, Collapsible, CollapsibleItem, Modal, Icon, Row, Input } from "react-materialize";
+import { Button, Collapsible, CollapsibleItem, Icon } from "react-materialize";
+import NewCity from "../NewCity";
+import NewNote from "../NewNote";
 
 
 const List = props => {
@@ -19,27 +21,13 @@ const List = props => {
                             {item.notes.map(note => {
                                 return (<p key={note}>{note}</p>)
                             })}
-                            <textarea rows="10" col="5" placeholder="Write a new note..."></textarea>
-                            <Button waves='light' onClick={() => console.log('add note')}>Add New Note<Icon right>send</Icon></Button>
+                            <NewNote />
                             <Button waves='light' onClick={() => console.log('remove city')}>Remove City<Icon right>delete</Icon></Button>
                         </CollapsibleItem>
                     );
                 })}
             </Collapsible>
-            <Modal
-                header='Add a New City to Your List'
-                trigger={<Button floating large className='red' waves='light' icon='add' />}>
-                <Row>
-                    <Input s={6} label="City Name" />
-                    <Input s={6} label="State" />
-                </Row>
-                Have you visited this city before?
-                <Row>
-                    <Input name='on' type='switch' value='1' />
-                    <Input name='group1' type='checkbox' value='red' label='Already Visited' />
-                </Row>
-                <Button waves='light' onClick={() => console.log('add city')}>Add City to List<Icon right>send</Icon></Button>
-            </Modal>
+            <NewCity />
         </div>
     );
 };
